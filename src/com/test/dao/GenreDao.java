cb45777da8b6cc515b99a13022e064a681165e7f
01438407 <01438407@PC01438407.corp.root>
2014-10-30 17:07:52 +0800

    just polish

--- src/com/test/dao/GenreDao.java
+++ src/com/test/dao/GenreDao.java
@@ -7,31 +7,9 @@ import com.test.model.Genre;
 
 public interface GenreDao {
 
-	//private JdbcTemplate jdbcTemplate;
 
-	//public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-	//	this.jdbcTemplate = jdbcTemplate;
-	//}
-	
 	public List<Genre> genreIndex();
-		
-		//String sql = "SELECT name FROM genre";
-		//List<Map<String,Object>> results = this.jdbcTemplate.queryForList(sql);
-		
-		//int rowCount = this.jdbcTemplate.queryForInt("SELECT COUNT(*) FROM ADDRESS");
-		//return results;	
-	//}
 	public List<Genre> genreBrowse(int genreId);
-		//##TODO
-		//String sql = "SELECT title FROM ALBUM WHERE genreId = "+genreId;
-		//List<Map<String,Object>> results = this.jdbcTemplate.queryForList(sql);
-		//return results;	
-	//}
-	//public int genreId(final String genre){
-	//	String sql = " = \"genre\"";
-	//	int g_id = this.jdbcTemplate.queryForInt("SELECT genreid from GENRE WHERE name =");
-	//	return g_id;
-	//}
 	public void genreCreate(Genre genre);
 	public void genreUpdate(Genre genre);
 	public void genreDelete(Long genreId);
